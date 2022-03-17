b46fdb507f13d893852a95aac5ebeb6e3ab25e2c
merenguessss <2681377430@qq.com>
2022-03-17 20:35:24 +0800

    feat: 调整KV处理方式，统一返回事件.

--- store/kvstore.go
+++ store/kvstore.go
@@ -3,24 +3,48 @@ package store
 import "github.com/cornelk/hashmap"
 
 type KV interface {
-	Set(string, *Node)
-	Get(string) (*Node, bool)
-	Delete(string)
+	Set(string, *Node) *Event
+	Get(string) (*Event, bool)
+	Delete(string) *Event
 }
 
 type kvstore struct {
 	m *hashmap.HashMap
 }
 
-func (kv *kvstore) Set(key string, value *Node) {
-	kv.m.Set(key, value)
-}
+func (kv *kvstore) Set(key string, value *Node) *Event {
+	event := &Event{
+		Name: Set,
+		Node: value,
+	}
 
-func (kv *kvstore) Get(key string) (*Node, bool) {
 	v, ok := kv.m.Get(key)
-	return v.(*Node), ok
+	if ok {
+		event.OldNode = v.(*Node)
+	}
+	kv.m.Set(key, value)
+
+	return event
 }
 
-func (kv *kvstore) Delete(key string) {
+func (kv *kvstore) Get(key string) (*Event, bool) {
+	v, ok := kv.m.Get(key)
+	return &Event{
+		Name: Get,
+		Node: v.(*Node),
+	}, ok
+}
+
+func (kv *kvstore) Delete(key string) *Event {
+	event := &Event{
+		Name: Set,
+	}
+
+	v, ok := kv.m.Get(key)
+	if ok {
+		event.OldNode = v.(*Node)
+	}
 	kv.m.Del(key)
+
+	return event
 }
