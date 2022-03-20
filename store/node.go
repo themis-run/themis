46b77721d1d560d776a709c264f04da8aad2ded3
merenguessss <2681377430@qq.com>
2022-03-20 17:06:18 +0800

    feat: 加入过期的判断以及处理.

--- store/node.go
+++ store/node.go
@@ -6,15 +6,33 @@ type Node struct {
 	Key   string
 	Value []byte
 
+	Expired    bool
 	CreateTime time.Time
+	ExpireTime time.Time
 	TTL        time.Duration
 }
 
 func newNode(key string, value []byte, ttl time.Duration) *Node {
-	return &Node{
+	now := time.Now()
+	n := &Node{
 		Key:        key,
 		Value:      value,
-		CreateTime: time.Now(),
+		Expired:    false,
+		CreateTime: now,
+		ExpireTime: now.Add(ttl),
 		TTL:        ttl,
 	}
+
+	n.ExpireTime = now.Add(ttl)
+
+	return n
+}
+
+func (n *Node) IsExpire() bool {
+	return time.Now().After(n.ExpireTime)
+}
+
+func (n *Node) UpdateTTL(ttl time.Duration) {
+	n.TTL = ttl
+	n.ExpireTime = time.Now().Add(ttl)
 }
