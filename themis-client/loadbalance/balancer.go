5b007e307eb213a13a4388745b169f14989e4651
merenguessss <2681377430@qq.com>
2022-03-31 16:22:43 +0800

    feat: 完善loadbalancer.

--- themis-client/loadbalance/balancer.go
+++ themis-client/loadbalance/balancer.go
@@ -5,12 +5,31 @@ import (
 	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().Unix())
+	Register(defaultName, &loadbalancer{})
+}
+
 type LoadBalancer interface {
 	Get(string, map[string]string, bool) string
 }
 
+var loadbalancerMap map[string]LoadBalancer
+var defaultName = "default"
+
 func New(name string) LoadBalancer {
+	if v, ok := loadbalancerMap[name]; ok {
+		return v
+	}
 	return &loadbalancer{}
+}
+
+func Register(name string, loadbalancer LoadBalancer) {
+	if loadbalancerMap == nil {
+		loadbalancerMap = make(map[string]LoadBalancer)
+	}
+
+	loadbalancerMap[name] = loadbalancer
 }
 
 type loadbalancer struct {
@@ -21,7 +40,6 @@ func (l *loadbalancer) Get(leaderName string, servers map[string]string, isWrite
 		return servers[leaderName]
 	}
 
-	rand.Seed(time.Now().Unix())
 	randomNum := rand.Intn(len(servers))
 
 	for _, v := range servers {
