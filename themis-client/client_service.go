e55ab017a17647f069d99231a450fc455a9d9d84
merenguessss <2681377430@qq.com>
2022-04-01 17:08:03 +0800

    fix: 修复服务注册的bug.

--- themis-client/client_service.go
+++ themis-client/client_service.go
@@ -5,9 +5,9 @@ import (
 	"time"
 )
 
-type Service struct {
-	ClusterName string
+type Instance struct {
 	ServiceName string
+	ClusterName string
 	IP          string
 	Port        int
 	TTL         time.Duration
@@ -19,82 +19,47 @@ const (
 	DefaultClusterName = "/"
 )
 
-func (c *Client) RegisterService(s *Service) error {
-	key := convertServiceKey(s.ClusterName, s.ServiceName)
-	return c.SetWithExpireTime(key, s, s.TTL)
+func (c *Client) RegisterInstanceWithIP(serviceName, ip string, port int) error {
+	return c.RegisterInstanceWithIPCluster(DefaultClusterName, serviceName, ip, port)
 }
 
-func (c *Client) DeleteService(serviceName string) error {
-	return c.DeleteServiceWithCluster(DefaultClusterName, serviceName)
-}
-
-func (c *Client) DeleteServiceWithCluster(clusterName, serviceName string) error {
-	key := convertServiceKey(clusterName, serviceName)
-	return c.Delete(key)
-}
-
-func (c *Client) DiscoverService(serviceName string) (*Service, error) {
-	return c.DiscoverServiceWithCluster(DefaultClusterName, serviceName)
-}
-
-func (c *Client) DiscoverServiceWithCluster(clusterName, serviceName string) (*Service, error) {
-	key := convertServiceKey(clusterName, serviceName)
-
-	kv, err := c.Get(key)
-	if err != nil {
-		return nil, err
+func (c *Client) RegisterInstanceWithIPCluster(serviceName, clusterName, ip string, port int) error {
+	instance := &Instance{
+		ClusterName: clusterName,
+		ServiceName: serviceName,
+		IP:          ip,
+		Port:        port,
 	}
-
-	s := &Service{}
-	if err := c.coder.Decode(kv.Value, s); err != nil {
-		return nil, err
-	}
-
-	return s, err
+	return c.RegisterInstance(instance)
 }
 
-func (c *Client) RegisterServiceWithHeartbeat(s *Service, heartbeatTimeout time.Duration) error {
-	if err := c.RegisterService(s); err != nil {
+func (c *Client) RegisterInstance(instance *Instance) error {
+	key := convertInstanceKey(instance.ServiceName, instance.ClusterName, instance.IP)
+	return c.SetWithExpireTime(key, instance, instance.TTL)
+}
+
+func (c *Client) RegisterInstanceWithHeartbeat(instance *Instance, heartbeatTimeout time.Duration) error {
+	if err := c.RegisterInstance(instance); err != nil {
 		return err
 	}
 
 	go func() {
-		for {
-			c.Heartbeat(s)
-			time.Sleep(heartbeatTimeout)
-		}
+		c.Heartbeat(instance)
+		time.Sleep(heartbeatTimeout)
 	}()
 
 	return nil
 }
 
-func (c *Client) Heartbeat(s *Service) error {
-	key := convertServiceKey(s.ClusterName, s.ServiceName)
-	return c.SetWithExpireTime(key, s, s.TTL)
+func (c *Client) Heartbeat(instance *Instance) error {
+	key := convertInstanceKey(instance.ServiceName, instance.ClusterName, instance.IP)
+	return c.SetWithExpireTime(key, instance, instance.TTL)
 }
 
-func (c *Client) WatchServiceStream(serviceName string, op OperateType, callback WatchCallback) error {
-	return c.WatchServiceStreamWithCluster(DefaultClusterName, serviceName, op, callback)
-}
-
-func (c *Client) WatchServiceStreamWithCluster(clusterName, serviceName string, op OperateType, callback WatchCallback) error {
-	key := convertServiceKey(clusterName, serviceName)
-	return c.WatchStream(key, op, callback)
-}
-
-func (c *Client) WatchService(serviceName string, op OperateType, callback WatchCallback) error {
-	return c.WatchServiceWithCluster(DefaultClusterName, serviceName, op, callback)
-}
-
-func (c *Client) WatchServiceWithCluster(clusterName, serviceName string, op OperateType, callback WatchCallback) error {
-	key := convertServiceKey(clusterName, serviceName)
-	return c.Watch(key, op, callback)
-}
-
-func convertServiceKey(clusterName, serviceName string) string {
+func convertInstanceKey(serviceName, clusterName, ip string) string {
 	if clusterName == "" {
 		clusterName = DefaultClusterName
 	}
 
-	return fmt.Sprintf("%s:%s:%s", ServiceMark, clusterName, serviceName)
+	return fmt.Sprintf("%s:%s:%s:%s", ServiceMark, serviceName, clusterName, ip)
 }
